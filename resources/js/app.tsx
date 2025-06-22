036d6e218d240994014406d17cebc11bafecf5b3
Ryuta Hamasaki <avosalmon@gmail.com>
2025-06-22 17:02:29 +0900

    Format

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -9,20 +9,20 @@ import { initializeTheme } from './hooks/use-appearance';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(
-            <AnimatePresence mode="wait">
-                <App key={props.initialPage.url} {...props} />
-            </AnimatePresence>,
-        );
-    },
-    progress: {
-        color: '#4B5563',
-    },
+    root.render(
+      <AnimatePresence mode="wait">
+        <App key={props.initialPage.url} {...props} />
+      </AnimatePresence>,
+    );
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
 
 // This will set light / dark mode on load...
